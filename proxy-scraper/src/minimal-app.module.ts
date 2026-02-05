163ecde8e87c2024e3b2deaf9231b9b1e71025ab
moweiran <iamlzy@163.com>
2026-02-05 09:28:22 +0800

    clean up

--- proxy-scraper/src/minimal-app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ScrapingService } from './services/scraping.service';
-import { SimpleKujialeService } from './services/simple-kujiale.service';
-
-@Module({
-  providers: [ScrapingService, SimpleKujialeService],
-  exports: [ScrapingService, SimpleKujialeService],
-})
-export class MinimalAppModule {}
